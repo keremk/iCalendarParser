3fd448c4a10a1f3e2ef001f3264eb0ee3cbfa89e
Kerem Karatal <kerem.karatal@soundcloud.com>
2016-12-29 18:33:11 +0100

    Add the parameter definitions from the iCalendar spec

--- iCalendarParser/Parser/ElementName.swift
+++ iCalendarParser/Parser/ElementName.swift
@@ -19,9 +19,24 @@ enum ElementName: String, RawRepresentable {
     case attendee = "ATTENDEE"
     case categories = "CATEGORIES"
     // MARK: Parameters
-    case altRep = "ALTREP"
-    case rsvp = "RSVP"
-    case cutype = "CUTYPE"
-    case cn = "CN"
+    case altRep = "ALTREP" // https://icalendar.org/iCalendar-RFC-5545/3-2-1-alternate-text-representation.html
+    case cutype = "CUTYPE" // https://icalendar.org/iCalendar-RFC-5545/3-2-3-calendar-user-type.html
+    case cn = "CN" // https://icalendar.org/iCalendar-RFC-5545/3-2-2-common-name.html
+    case delegatedFrom = "DELEGATED-FROM" // https://icalendar.org/iCalendar-RFC-5545/3-2-4-delegators.html
+    case delegatedTo = "DELEGATED-TO" // https://icalendar.org/iCalendar-RFC-5545/3-2-5-delegatees.html
+    case dir = "DIR"  // https://icalendar.org/iCalendar-RFC-5545/3-2-6-directory-entry-reference.html
+    case formatType = "FMTTYPE" // https://icalendar.org/iCalendar-RFC-5545/3-2-8-format-type.html
+    case freeBusyType = "FBTYPE" // https://icalendar.org/iCalendar-RFC-5545/3-2-9-free-busy-time-type.html
+    case language = "LANGUAGE" // https://icalendar.org/iCalendar-RFC-5545/3-2-10-language.html
+    case member = "MEMBER" // https://icalendar.org/iCalendar-RFC-5545/3-2-11-group-or-list-membership.html
+    case participationStatus = "PARTSTAT" // https://icalendar.org/iCalendar-RFC-5545/3-2-12-participation-status.html
+    case range = "RANGE" // https://icalendar.org/iCalendar-RFC-5545/3-2-13-recurrence-identifier-range.html
+    case related = "RELATED" // https://icalendar.org/iCalendar-RFC-5545/3-2-14-alarm-trigger-relationship.html
+    case relType = "RELTYPE" // https://icalendar.org/iCalendar-RFC-5545/3-2-15-relationship-type.html
+    case role = "ROLE" // https://icalendar.org/iCalendar-RFC-5545/3-2-16-participation-role.html
+    case rsvp = "RSVP" // https://icalendar.org/iCalendar-RFC-5545/3-2-17-rsvp-expectation.html
+    case sentBy = "SENT-BY" // https://icalendar.org/iCalendar-RFC-5545/3-2-18-sent-by.html
+    case tzid = "TZID" // https://icalendar.org/iCalendar-RFC-5545/3-2-19-time-zone-identifier.html
+    case value = "VALUE" // https://icalendar.org/iCalendar-RFC-5545/3-2-20-value-data-types.html
 }
 
--- iCalendarParser/Parser/RuleSet.swift
+++ iCalendarParser/Parser/RuleSet.swift
@@ -18,8 +18,23 @@ struct RuleSet {
         .description: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .property),
         .categories: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .property, isMultiValued: true),
         .altRep: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
-        .rsvp: DefaultRule(valueMapper: AnyValueMapper(BooleanMapper()), nodeType: .parameter),
         .cutype: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
-        .cn: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter)
+        .cn: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .delegatedFrom: DefaultRule(valueMapper: AnyValueMapper(CalendarUserAddressMapper()), nodeType: .parameter),
+        .delegatedTo: DefaultRule(valueMapper: AnyValueMapper(CalendarUserAddressMapper()), nodeType: .parameter, isMultiValued: true),
+        .dir: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .formatType: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .freeBusyType: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .language: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .member: DefaultRule(valueMapper: AnyValueMapper(CalendarUserAddressMapper()), nodeType: .parameter),
+        .participationStatus: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .range: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .related: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .relType: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .role: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .rsvp: DefaultRule(valueMapper: AnyValueMapper(BooleanMapper()), nodeType: .parameter),
+        .sentBy: DefaultRule(valueMapper: AnyValueMapper(CalendarUserAddressMapper()), nodeType: .parameter),
+        .tzid: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter),
+        .value: DefaultRule(valueMapper: AnyValueMapper(TextMapper()), nodeType: .parameter)
     ]
 }
